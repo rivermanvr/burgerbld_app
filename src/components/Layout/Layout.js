2fb28e8d329d9c5061ce2705cda0112bf9ba4d7a
rivermanvr <vrgithub@riversconsulting.com>
2018-03-22 15:35:23 -0400

    setting up core components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,3 +1,10 @@
-const layout = () => {
-  <div>Toolbar, SideDrawer, Backdrop</div>
-}
+import React from 'react';
+
+const layout = (props) => (
+  <>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>
+    { props.children }
+    </main>
+  </>
+)
