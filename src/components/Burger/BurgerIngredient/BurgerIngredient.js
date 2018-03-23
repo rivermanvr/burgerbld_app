536c9bfc6cb5950e007c48815eb6f981fc1a7462
rivermanvr <vrgithub@riversconsulting.com>
2018-03-23 16:24:22 -0400

    setup switch for ingredients....

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -10,9 +10,28 @@ const BurgerIngredient = (props) => {
       break;
     case ('bread-top'):
       ingredient = (
-        <div className={  }</div>
+        <div className={ classes.BreadTop }>
+          <div className={ classes.Seeds1 }></div>
+          <div className={ classes.Seeds2 }></div>
+        </div>
       );
+      break;
+    case ('meat'):
+      ingredient = <div className={ classes.Meat }></div>;
+      break;
+    case ('cheese'):
+      ingredient = <div className={ classes.Cheese }></div>;
+      break;
+    case ('salad'):
+      ingredient = <div className={ classes.Salad }></div>;
+      break;
+    case ('bacon'):
+      ingredient = <div className={ classes.Bacon }></div>;
+      break;
+    default:
+      ingredient = null;
   }
+  return ingredient;
 }
 
 export default BurgerIngredient;
