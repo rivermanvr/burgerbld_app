3126d7abc650e41cf1cac5b7dae69e6f44da76c0
rivermanvr <vrgithub@riversconsulting.com>
2018-03-23 11:10:11 -0400

    laying out the scheleton for our little app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BergerBuilder from './containers/BergerBuilder/BergerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>test</p>
+          <BergerBuilder />
         </Layout>
       </div>
     );
