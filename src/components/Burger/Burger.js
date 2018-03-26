d518338c606b406cb01b8508aed92483ebe94527
rivermanvr <vrgithub@riversconsulting.com>
2018-03-26 19:52:39 -0400

    introduced State to BurgerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingrKey => {
+      return [...Array(props.ingredients[ingrKey])].map( (_, index) => {
+        return <BurgerIngredient key={ ingrKey + index } type={ ingrKey } />
+      })
+    });
+
   return (
     <div className={ classes.Burger }>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      { transformedIngredients }
       <BurgerIngredient type="bread-bottom" />   
     </div>
   );
