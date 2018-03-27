8552adb2f206633599e04eadce2ab9f04909005a
rivermanvr <vrgithub@riversconsulting.com>
2018-03-27 11:33:00 -0400

    built BuildControl component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -12,7 +12,7 @@ const burger = (props) => {
     .reduce( (arr, el) => {
       return arr.concat(el);
     }, []);
-    if (transformedIngredients === 0) {
+    if (transformedIngredients.length === 0) {
       transformedIngredients = <p>Please start adding ingredients</p>
     }
   return (
