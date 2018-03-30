8b38188d58637f3d798fac237e94f30ed5b22311
rivermanvr <vrgithub@riversconsulting.com>
2018-03-30 13:18:28 -0400

    created and added Modal to BurgerBuilder

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Modal.css';
 
-const modal = (props) => {
-  <div>
+const modal = (props) => (
+  <div className={ classes.Modal }>
     { props.children }
   </div>
-}
+)
 
 export default modal;
