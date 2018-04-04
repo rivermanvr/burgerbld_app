39fdc980d005b70dd7e55ae9ffc5e906063b3445
rivermanvr <vrgithub@riversconsulting.com>
2018-04-04 19:55:33 -0400

    skeleton for NAV/Toolbar component done with placeholders

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const toolbar = (props) => (
+  <header>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>
+      <ul>
+        ...
+      </ul>
+    </nav>
+  </header>
+);
+
+export default toolbar;
