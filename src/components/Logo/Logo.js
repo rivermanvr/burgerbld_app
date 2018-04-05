8441ebf653410d4a19b106c7b991a8833d682cef
rivermanvr <vrgithub@riversconsulting.com>
2018-04-05 13:21:13 -0400

    logo implemented

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import RivConLogo from '../../assets/images/RivConLogo.png';
+import classes from './Logo.css';
 
 const logo = (props) => (
-
+  <div className={ classes.Logo }>
+    <img src={ RivConLogo } alt="MyBurgerBld" />
+  </div>
 );
 
 export default logo;
