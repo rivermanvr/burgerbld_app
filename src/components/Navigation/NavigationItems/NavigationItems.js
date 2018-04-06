4846a7f955a90e9e586509be38e414a88352d9eb
rivermanvr <vrgithub@riversconsulting.com>
2018-04-06 14:11:31 -0400

    setting up the navigation components

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+  <ul>
+    <NavigationItem />
+  </ul>
+);
+
+export default navigationItems;
