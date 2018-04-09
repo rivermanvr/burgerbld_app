322875a92aab34adc53227e96e41e69e6b37d351
rivermanvr <vrgithub@riversconsulting.com>
2018-04-09 12:43:00 -0400

    synchronizing offline/online work

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={ classes.content }>
     { props.children }
     </main>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import classes from './NavigationItems.css';
 
 const navigationItems = () => (
   <ul className={ classes.NavigationItems }>
-    <NavigationItem link="/">Burger Builder</NavigationItem>
+    <NavigationItem link="/" active>Burger Builder</NavigationItem>
     <NavigationItem link="/">Checkout</NavigationItem>
   </ul>
 );
