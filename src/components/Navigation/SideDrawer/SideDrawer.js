04aa0bbfd46e5ba38ea6652d551ae718cf8153d7
rivermanvr <vrgithub@riversconsulting.com>
2018-04-09 13:35:55 -0400

    added Backdop component behind SideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
   return (
-    <div className={ classes.SideDrawer }>
-      <div className={ classes.Logo }>
-        <Logo />   
+    <Aux>
+      <Backdrop />
+      <div className={ classes.SideDrawer }>
+        <div className={ classes.Logo }>
+          <Logo />   
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
