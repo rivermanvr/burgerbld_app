1dd12f4ed33c1eb02ce1c50a161611ef87235c6e
rivermanvr <vrgithub@riversconsulting.com>
2018-04-09 12:59:01 -0400

    responsive work done on logo (toolBar & SideDrawer)

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,8 +2,15 @@ import React from 'react';
 import RivConLogo from '../../assets/images/RivConLogo.png';
 import classes from './Logo.css';
 
+// notice the style change in the wrapping div - this is a way to 
+// control responsive rendering.  We can pass props from the component
+// Note: I am going to solve this issue a different way and
+// left the "style" to remind myself of this alternative solution
+// see: originating components:  SideDrawer & ToolBar
+// solution: notice the wrapping div around the Logo component call. 
+
 const logo = (props) => (
-  <div className={ classes.Logo }>
+  <div className={ classes.Logo } style={ { height: props.height } }>
     <img src={ RivConLogo } alt="MyBurgerBld" />
   </div>
 );
