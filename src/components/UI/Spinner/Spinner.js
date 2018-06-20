119a1698d0da089a2f117b0e347c211fb937f903
rivermanvr <vrgithub@riversconsulting.com>
2018-06-20 14:11:57 -0400

    added address of spinner site.

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import classes from './Spinner.css';
 
+// https://projects.lukehaas.me/css-loaders/
+
 const spinner = () => (
   <div className={ classes.Loader }>Loading...</div>
 );
