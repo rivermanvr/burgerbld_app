9347da239d83588a9c50d48a9ea27a6d0a600cb9
rivermanvr <vrgithub@riversconsulting.com>
2018-06-21 10:53:11 -0400

    set up a global error handler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,3 +1,17 @@
 import React from 'react';
 
-const withErrorHandler ()
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Aux/Aux';
+
+const withErrorHandler = (WrappedComponent) => {
+  return (props) => {
+    return (
+      <Aux>
+        <Modal>Something did not work!</Modal>
+        <WrappedComponent { ...props } />
+      </Aux>
+    );
+  }
+};
+
+export default withErrorHandler
